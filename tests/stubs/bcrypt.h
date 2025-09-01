0b47d87553e358adf15a9774d2beed3a08e1a62b
ZilverZtream <zilverztream@gmail.com>
2025-09-01 10:58:25 +0200

    Remove compiled test binary

---
+++ tests/stubs/bcrypt.h
@@ -0,0 +1,19 @@
+#ifndef BCRYPT_H
+#define BCRYPT_H
+#include "windows.h"
+
+typedef void* BCRYPT_ALG_HANDLE;
+typedef void* BCRYPT_HASH_HANDLE;
+
+#define BCRYPT_SHA1_ALGORITHM L"SHA1"
+#define BCRYPT_OBJECT_LENGTH L"ObjectLength"
+
+int BCryptOpenAlgorithmProvider(BCRYPT_ALG_HANDLE* phAlg, const wchar_t* algId, const wchar_t* implementation, unsigned long flags);
+int BCryptGetProperty(BCRYPT_ALG_HANDLE hAlg, const wchar_t* propId, unsigned char* pbOutput, unsigned long cbOutput, unsigned long* pcbResult, unsigned long flags);
+int BCryptCreateHash(BCRYPT_ALG_HANDLE hAlg, BCRYPT_HASH_HANDLE* phHash, unsigned char* pbHashObject, unsigned long cbHashObject, unsigned char* pbSecret, unsigned long cbSecret, unsigned long flags);
+int BCryptHashData(BCRYPT_HASH_HANDLE hHash, unsigned char* pbInput, unsigned long cbInput, unsigned long flags);
+int BCryptFinishHash(BCRYPT_HASH_HANDLE hHash, unsigned char* pbOutput, unsigned long cbOutput, unsigned long flags);
+int BCryptDestroyHash(BCRYPT_HASH_HANDLE hHash);
+int BCryptCloseAlgorithmProvider(BCRYPT_ALG_HANDLE hAlg, unsigned long flags);
+
+#endif
---
+++ tests/stubs/shlwapi.h
@@ -0,0 +1,5 @@
+#ifndef SHLWAPI_H
+#define SHLWAPI_H
+#include "windows.h"
+BOOL PathIsUNCW(const wchar_t* path);
+#endif
